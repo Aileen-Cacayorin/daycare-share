2e0d8a494b5cfd5471284310c84518c35cec5102
Aileen <aileen.cacayorin@gmail.com>
2015-10-07 15:07:50 -0700

    add class form

--- app/components/class-modal.js
+++ app/components/class-modal.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  actions: {
+    addClass() {
+      var params = {
+        name: this.get('className'),
+        ages: this.get('ages'),
+        description: this.get('classDescription'),
+        schedule: this.get('schedule'),
+        image: this.get('image'),
+        daycare: this.get('daycare')
+
+      }
+    }
+  }
 });
